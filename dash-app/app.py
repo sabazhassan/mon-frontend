92427f023184871843c5faac6a3b71cd669941c0
jenspfeifle <jens@pfeifle.tech>
2020-05-06 14:09:18 +0200

    make pylint happy

--- dash-app/app.py
+++ dash-app/app.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+"""
+Main dash app
+"""
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(
+    __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+)
 
 app.layout = html.Div(
     children=[
